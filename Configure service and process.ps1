4fe3d107fb4e6f4fcd075ac5a241ba54a23f8458
DarkLite1 <BrechtGijbels@gmail.com>
2023-07-11 11:41:33 +0200

    add ScriptAdmin backup

--- Configure service and process.ps1
+++ Configure service and process.ps1
@@ -38,7 +38,10 @@ Param(
     [Parameter(Mandatory)]
     [String]$ImportFile,
     [String]$LogFolder = "$env:POWERSHELL_LOG_FOLDER\Monitor\Monitor service\$ScriptName",
-    [String[]]$ScriptAdmin = $env:POWERSHELL_SCRIPT_ADMIN
+    [String[]]$ScriptAdmin = @(
+        $env:POWERSHELL_SCRIPT_ADMIN,
+        $env:POWERSHELL_SCRIPT_ADMIN_BACKUP
+    )
 )
 
 Begin {
